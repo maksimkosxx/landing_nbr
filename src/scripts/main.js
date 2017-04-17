1f0653ff47e820719017abee9b89a5dc7c204747
MScheglov <maksimkosxx@gmail.com>
2017-04-17 22:33:20 +0400

    блок hero

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,35 @@
 $(document).ready(function(){
 
-
+    $(function(){
+        //2. Получить элемент, к которому необходимо добавить маску
+        $('.hero-content__form input[type="text"]').mask("+7 (999) 999-99-99");
+    });
 
     // Бургер
 
-    $('.menu__btn').on("click", function () {
-        $('.menu__navigation').slideToggle();
-        $('.menu__btn').toggleClass('active');
-    });
-
-    $( function() {
-        $('.price-switches').accordion({
-            collapsible: true,
-            heightStyle: "content"
-        });
-    } );
-
-    $('.price-switches__service').on("click", function() {
-        var click_id=$(this).attr('id');
-        if (click_id != $('.price-switches__service.active').attr('id') ) {
-            $('.price-switches__service').removeClass('active');
-            $(this).addClass('active');
-            $('.price-content').removeClass('active');
-            $('#item_' + click_id).addClass('active');
-
-        }
-        return false;
-    });
+    // $('.menu__btn').on("click", function () {
+    //     $('.menu__navigation').slideToggle();
+    //     $('.menu__btn').toggleClass('active');
+    // });
+    //
+    // $( function() {
+    //     $('.price-switches').accordion({
+    //         collapsible: true,
+    //         heightStyle: "content"
+    //     });
+    // } );
+    //
+    // $('.price-switches__service').on("click", function() {
+    //     var click_id=$(this).attr('id');
+    //     if (click_id != $('.price-switches__service.active').attr('id') ) {
+    //         $('.price-switches__service').removeClass('active');
+    //         $(this).addClass('active');
+    //         $('.price-content').removeClass('active');
+    //         $('#item_' + click_id).addClass('active');
+    //
+    //     }
+    //     return false;
+    // });
 
 
 
