28549d5f8196795ed0c5ef88764708fa86574d95
Максим Щеглов <scheglov@kefir-media.ru>
2017-04-19 11:09:19 +0300

    Адаптация

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
     // Маска поля
 
     $(function(){
-        $('#id_phone').mask("+7 (999) 999-99-99");
+        $('input[type="text"]').mask("+7 (999) 999-99-99");
     });
 
     //Скролл к форме заявки
 
 
-    $('.brands-btn, .advantages-btn').on('click', function (event) {
+    $('.brands-btn, .advantages-btn, .diagnostics-btn').on('click', function (event) {
         event.preventDefault();
 
         var id  = $(this).attr('href'),
@@ -18,32 +18,26 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 
+    // Прогресс-бар онлайн диагностика
 
-    $('input').on('click', function(){
-        var valeur = 0;
-        $('input:checked').each(function(){
-            if ( $(this).attr('value') > valeur )
-            {
-                valeur =  $(this).attr('value');
-            }
-        });
-        $('.progress-bar').css('width', valeur+'%').attr('aria-valuenow', valeur);
+    $('.diagnostics-list li').on('click', function(){
+
+        $('.diagnostics-list__item').removeClass('active');
+        $(this).addClass('active');
+
+        var number = $(this).attr('value');
+
+        if($(this).hasClass('active')) {
+            $('.progress-bar').css({'width': number});
+            $('.diagnostics-value').html(number);
+        }
+
     });
 
-    // Бургер
 
-    // $('.menu__btn').on("click", function () {
-    //     $('.menu__navigation').slideToggle();
-    //     $('.menu__btn').toggleClass('active');
-    // });
-    //
-    // $( function() {
-    //     $('.price-switches').accordion({
-    //         collapsible: true,
-    //         heightStyle: "content"
-    //     });
-    // } );
-    //
+    $('.hero-content__image img').fadeIn(1000);
+
+
 
 
 });
